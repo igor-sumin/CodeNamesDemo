f26c49a93223a2d39883578870729a857a4bcf73
Dmitry Mordvinov <d.mordvinov@gmail.com>
2021-07-29 12:28:29 +0300

    set allo-origins by env setting

--- src/main/java/com/netcracker/filters/CodeNamesCorsFilter.java
+++ src/main/java/com/netcracker/filters/CodeNamesCorsFilter.java
@@ -1,5 +1,6 @@
 package com.netcracker.filters;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,14 @@ import java.io.IOException;
 @Order(1)
 public class CodeNamesCorsFilter implements Filter {
 
+    private final String corsAllowOrigin;
+
+    public CodeNamesCorsFilter(@Value("${codenames.cors-allow-origin}") String corsAllowOrigin) {
+        this.corsAllowOrigin = corsAllowOrigin;
+    }
+
+    
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
@@ -19,7 +28,7 @@ public class CodeNamesCorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        response.setHeader("Access-Control-Allow-Origin", corsAllowOrigin);
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods",
                 "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PATCH, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
