b9bbab4d0ed61b62d31f20da41ee58fa19b59856
Dmitry Mordvinov <d.mordvinov@gmail.com>
2021-07-29 22:58:21 +0300

    fix production

--- src/main/java/com/netcracker/controllers/entry/LoginController.java
+++ src/main/java/com/netcracker/controllers/entry/LoginController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("login")
+@RequestMapping("/login")
 public class LoginController {
 
     private final LoginService loginService;
